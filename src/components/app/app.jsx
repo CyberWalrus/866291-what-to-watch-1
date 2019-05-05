0d8d1b32a229ba09636fafb061cf97187104c3f4
Cyber Walrus <stonewalrusfan@gmail.com>
2019-05-05 16:50:59 +0300

    add main page

---
+++ src/components/app/app.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import MainScreen from "../main-screen/main-screen.jsx";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <MainScreen />;
+  }
+}
