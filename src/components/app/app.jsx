a3e38249522283dcf5366d40c6b561aacaa2a1ed
Cyber Walrus <stonewalrusfan@gmail.com>
2019-05-09 19:31:59 +0300

    add array film name and proptypes

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,8 +4,11 @@ import MainScreen from "../main-screen/main-screen.jsx";
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      films: [`Fantastic Beasts`, `Bohemian Rhapsody`, `Macbeth`, `We need to talk about Kevin`]
+    };
   }
   render() {
-    return <MainScreen />;
+    return <MainScreen films={this.state.films}/>;
   }
 }
